7f7bdb8e1809369599f59b540727f5e80e714b31
Thomas Fournier <thomasfournierbrest@gmail.com>
2023-09-08 19:23:49 +0200

    Add modal and connexion

--- src/components/SignUpModal.js
+++ src/components/SignUpModal.js
@@ -1,7 +1,57 @@
+import React, {useContext, useRef, useState} from "react"
+import {UserContext} from "../context/userContext"
+
 export default function SignUpModal() {
+
+    const {modalState, toggleModals, signUp} = useContext(UserContext)
+
+    console.log(signUp);
+
+    const [validation, setValidation] = useState("");
+
+    const inputs = useRef([])
+    const addInputs = el => {
+        if(el && !inputs.current.includes(el)) {
+            inputs.current.push(el)
+        }
+    }
+
+    const formRef = useRef();
+
+    const handleForm = async e => {
+        e.preventDefault()
+        
+        if((inputs.current[1].value.length || inputs.current[2].value.length) < 6) {
+            setValidation("6 characters minimum")
+            return;
+        } else if(inputs.current[1].value !== inputs.current[2].value){
+            setValidation("Passwords do not match")
+            return;
+        }
+
+        try {
+            const cred = await signUp(
+                inputs.current[0].value,
+                inputs.current[1].value
+            )
+            formRef.current.reset();
+            setValidation("")
+            console.log(cred)
+            
+        } catch(err) {
+
+        }
+    }
+
+
   return (
+    <>
+    {modalState.signUpModal && (
     <div className="position-fixed top-0 vw-100 vh-100">
-        <div className="w-100 h-100 bg-dark bg-opacity-75">
+        <div 
+        onClick={() => toggleModals("close")}
+        className="w-100 h-100 bg-dark bg-opacity-75">
+        </div>
 
             <div 
             className="position-absolute top-50 start-50 translate-middle"
@@ -10,25 +60,30 @@ export default function SignUpModal() {
                     <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title">Sign up</h5>
-                            <button className="btn-close"></button>
+                            <button 
+                            onClick={() => toggleModals("close")}
+                            className="btn-close"></button>
                         </div>
 
                         <div className="modal-body">
-                            <form className="sign-up-form">
+                            <form
+                            ref={formRef}
+                            onSubmit={handleForm}
+                            className="sign-up-form">
                                 <div className="mb-3">
                                     <label htmlFor="signUpEmail" className="form-label">Email adress</label>
-                                    <input type="email" className="form-control" name="email" required id="signUpEmail" />
+                                    <input ref={addInputs} type="email" className="form-control" name="email" required id="signUpEmail" />
                                 </div>
 
                                 <div className="mb-3">
                                     <label htmlFor="signUpPwd" className="form-label">Password</label>
-                                    <input type="password" className="form-control" name="pwd" required id="signUpPwd" />
+                                    <input  ref={addInputs} type="password" className="form-control" name="pwd" required id="signUpPwd" />
                                 </div>
 
                                 <div className="mb-3">
                                     <label htmlFor="repeatPwd" className="form-label">Repeat password</label>
-                                    <input type="password" className="form-control" name="pwd" required id="repeatPwd "/>
-                                    <p className="text-danger mt-1">Validation</p>
+                                    <input ref={addInputs} type="password" className="form-control" name="pwd" required id="repeatPwd "/>
+                                    <p className="text-danger mt-1">{validation}</p>
                                 </div>
 
                                 <button className="btn btn-primary">Submit</button>
@@ -39,6 +94,8 @@ export default function SignUpModal() {
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )}
+    </>
+  );
+
 }
